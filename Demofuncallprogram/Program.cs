cfc03014fa1ea826da68b8bc91fd935c986f1a03
Pallavikothawade <Pallavijkothawade@gmail.com>
2022-12-12 13:19:32 +0530

    Wlcome the demofunction

---
+++ Demofuncallprogram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demofuncallprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Everyone");
+            calculator c = new calculator();
+            c.Addition();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demofuncallprogram/calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demofuncallprogram
+{
+    internal class calculator
+    {
+        public void Addition()
+        {
+            int firstnumber = 0, secondnumber = 0;
+
+            Console.WriteLine("Plz enter the 1st number");
+            firstnumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Plz enter the 2nd number");
+            secondnumber = Convert.ToInt32(Console.ReadLine());
+
+            int result = firstnumber + secondnumber;
+            Console.WriteLine("sum of two number" + result);
+        }
+    }
+}
